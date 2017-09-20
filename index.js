5e094de883a181dd995f3cf2951e578cfebd9952
Pablo <pawel.stolka@gmail.com>
2017-09-20 21:43:58 +0200

    /surprise

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/surprise', function(req, res) {
+    res.send('Paweł przesyła Basi moc buziaków!  cmok! cmok! cmok!!!');
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
