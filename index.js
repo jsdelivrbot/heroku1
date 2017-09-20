d1bbb06182363f82fd89f021eafc8a4ee00deed5
Pablo <pawel.stolka@gmail.com>
2017-09-20 22:04:18 +0200

    oczy

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
+var cool = require('cool-ascii-faces');
+// cors = require('cors'),
+// mongoose = require('mongoose'),
+// bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,8 +16,27 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
+
+app.get('/getCup', function(req, res) {
+    fs.readFile('./public/users.json', 'utf-8', function(err, data) {
+        if (err) throw err;
+
+        var jsonArr = JSON.parse(data);
+        // var newUser = {
+        // 	name: 'pablito',
+        // 	email: 'pablo.ciento@gmail.com'
+        // }
+        // jsonArr.users.push(newUser);
+        console.log(jsonArr);
+
+        fs.writeFile('./public/users.json', JSON.stringify(jsonArr), 'utf-8', function(err) {
+            if (err) throw err;
+            console.log('Done!')
+        })
+    })
+})
 
 app.get('/surprise', function(req, res) {
     res.send('Paweł przesyła Basi moc buziaków!  cmok! cmok! cmok!!!');
@@ -21,6 +46,10 @@ app.get('/czykocha', function(req, res) {
     res.send('Paweł kocha bardzo Basię!!!');
 })
 
+app.get('/oczy', function(req, res) {
+    res.send(cool());
+})
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
